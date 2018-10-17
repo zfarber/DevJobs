8517570381678641ccb79634ea40134e85bc1c0d
Zach Farber <zfarber97@gmail.com>
2018-10-17 09:10:20 -0400

    got request to github jobs working... using a CORS chrome extension

--- devjobs/src/App/index.jsx
+++ devjobs/src/App/index.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+
+import Jobs from '../Jobs';
+
 import "./style.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return <Jobs />;
   }
 }
 
