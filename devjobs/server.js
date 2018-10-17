4e969a6ce9e8a1c70f09b0a9bb0f67a75c5e6a61
Shaun Dawson <shaundawson0883@gmail.com>
2018-10-17 14:28:15 -0400

    installed webpack confirmed working routes

---
+++ devjobs/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const hotMiddleware     = require('./environment');
+
+
+// start express
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+
+
+app.use(hotMiddleware(app.get('env')));
+
+// ROUTE HANDLERS
+
+
+
+// GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
+
+
+// START SERVER
+app.listen(app.get('port'), () => {
+  console.log(`Server up and listening on port ${app.get('port')}, in ${app.get('env')} mode.`);
+}).on('error', console.error);
+
+module.exports = app;
