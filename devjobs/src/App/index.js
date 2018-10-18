21d7fe652045aafe33d392103ddd7e748c7ea135
Zach Farber <zfarber97@gmail.com>
2018-10-18 10:15:20 -0400

    setup CreateUserPage

--- devjobs/src/App/index.js
+++ devjobs/src/App/index.js
@@ -3,6 +3,7 @@
 
   import HomePage from '../HomePage';
   import Jobs from '../Jobs';
+  import CreateUserPage from '../CreateUserPage';
 
   class App extends Component {
     render() {
@@ -11,6 +12,7 @@
             <Switch>
               <Route path="/" exact component={ HomePage } />
               <Route path='/jobs' component={ Jobs }/>
+              <Route path='/newuser' component={ CreateUserPage } />
             </Switch>
           </div>
       );
