67e996f4026eda50ae0380e7bad80dc50ee5ae79
Shaun Dawson <shaundawson0883@gmail.com>
2018-10-19 10:50:50 -0400

    updated logo and navbar component

--- devjobs/src/Nav/index.js
+++ devjobs/src/Nav/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarItem,
-  NavbarStart,
-  NavbarEnd,
-  NavbarLink,
-  NavbarDropdown,
-  NavbarDivider,
-} from 'bloomer';
-
+import brand from './logo.png';
 
 export default () => (
 <div className="hero-head">
-            <nav className="navbar is-dark">
+            <nav className="navbar is-black">
                 <div className="container">
                     <div className="navbar-brand">
                         <a className="navbar-item" href="/">
-                            <img src="./images/misc/logo.png" className="img-responsive" alt="Logo" />
+                            <img src={brand} className="img-responsive" alt="Logo" />
                         </a>
                         <span className="navbar-burger burger" data-target="navbarMenu">
                             <span></span>
@@ -28,13 +18,13 @@ export default () => (
                     <div id="navbarMenu" className="navbar-menu">
                         <div className="navbar-end">
                             <span className="navbar-item">
-                                <a className="button is-white is-outlined" href="#">
-                                    <span>Recruiters</span>
+                                <a className="button is-white is-outlined" href="#/login">
+                                    <span>Login</span>
                                 </a>
                             </span>
                             <span className="navbar-item">
-                                <a className="button is-white is-outlined" href="#">
-                                    <span>Job Seekers</span>
+                                <a className="button is-white is-outlined" href="#/signup">
+                                    <span>Sign Up</span>
                                 </a>
                             </span>
                         </div>
